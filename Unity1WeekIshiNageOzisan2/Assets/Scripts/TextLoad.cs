c89301e71a8a4aba9c5f6c7f1d3f511b9d311936
DESKTOP-K27J26M\o_10s <DeadMansSun@gmail.com>
2017-05-16 01:23:50 +0900

    テキストデータをロードすることスクリプトを作成した

---
+++ Unity1WeekIshiNageOzisan2/Assets/Scripts/TextLoad.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextLoad : MonoBehaviour {
+
+    [SerializeField]
+    private int mapWidth;
+    [SerializeField]
+    private int mapLength;
+    [SerializeField]
+    private string textMapData;
+
+    public GameObject prefab_block;
+    public string mapDataFilePath;
+
+    public string[] p_mapData;
+
+	// Use this for initialization
+	void Start () {
+        textMapData = Resources.Load(mapDataFilePath).ToString();
+        LoadMapData();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// マップデータをロードする.
+    /// </summary>
+    public void LoadMapData()
+    {
+        //縦幅を改行コードで区切ることで取得する.
+        string[] mapDataLength = textMapData.Split(new string[] { "\r", "\n" }, System.StringSplitOptions.RemoveEmptyEntries);
+        //Debug.Log(line[1]);
+        //横幅を","で区切ることで取得する.
+        string[] mapDataWidth = mapDataLength[0].Split(new string[] {","}, System.StringSplitOptions.RemoveEmptyEntries);
+        int mapWidth = mapDataWidth.Length;
+        int mapLength = mapDataLength.Length;
+        //全部の要素をばらす.
+        string[] mapData = textMapData.Split(new string[] {",","\r","\n"},System.StringSplitOptions.RemoveEmptyEntries);
+        p_mapData = mapData;
+    }
+
+}
