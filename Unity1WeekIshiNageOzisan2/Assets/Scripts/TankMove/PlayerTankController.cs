4043ab49eb78e84a5ecfde18e05d2f66c87abe64
DESKTOP-K27J26M\o_10s <DeadMansSun@gmail.com>
2017-05-19 01:26:34 +0900

    キャラの移動についてのスクリプトを作成

---
+++ Unity1WeekIshiNageOzisan2/Assets/Scripts/TankMove/PlayerTankController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTankController : MonoBehaviour {
+
+    [SerializeField]
+    private TankMove tankMove;
+    private Vector3 moveDirection;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Horizontal") !=0||Input.GetAxis("Vertical")!=0) {
+            InputPlayer();
+        }
+    }
+
+    void InputPlayer()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        moveDirection = new Vector3(x,transform.position.y,z);
+        tankMove.Move(moveDirection,x,z);
+    }   
+
+
+}
---
+++ Unity1WeekIshiNageOzisan2/Assets/Scripts/TankMove/TankMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMove : MonoBehaviour {
+
+    [SerializeField]
+    private CreateMapData createMapData;
+    private float mapTipSize;
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private float rotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+        mapTipSize = createMapData.setOffSetValue;
+    }
+
+    public void Move(Vector3 inputDirection,float inputX,float inputZ)
+    {
+        Vector3 nextPos = transform.position;
+        nextPos.x += inputDirection.x + mapTipSize * inputX;
+        nextPos.z += inputDirection.z + mapTipSize * inputZ;
+        Rotate(nextPos);
+        transform.position = Vector3.Slerp(transform.position,nextPos,moveSpeed/60);
+    }
+
+    void Rotate(Vector3 nextPos)
+    {
+        /* angleは使いにくい.
+        float angle = Vector3.Angle(transform.forward,direction);
+        Debug.Log(angle);
+        transform.Rotate(transform.up,angle);
+        */
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(nextPos - transform.position),rotateSpeed);
+    }
+
+}
